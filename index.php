6ca92ee01f3253f0589bd16771ee1e6fe51ba120
Pol <polcerdan@gmail.com>
2018-01-23 00:25:10 +0100

    integració carrousel versio 1

--- index.php
+++ index.php
@@ -4,6 +4,9 @@
 <!-- Correcció viewport versions movils -->
 <meta name="viewport" content="width=device-width, initial-scale=0.9 user-scalable=no">
 
+<!-- JQuery --> 
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+
 <!-- Latest compiled and minified CSS -->
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
 
@@ -49,6 +52,43 @@
 		</div>		
 	</div>
 </header>
+<div class="row separar">
+	<div id="myCarousel" class="carousel slide" data-ride="carousel">
+	  <!-- Indicators -->
+	  <ol class="carousel-indicators">
+		<li data-target="#myCarousel" data-slide-to="0" class="active"></li>
+		<li data-target="#myCarousel" data-slide-to="1"></li>
+		<li data-target="#myCarousel" data-slide-to="2"></li>
+		<li data-target="#myCarousel" data-slide-to="3"></li>
+	  </ol>
+
+	  <!-- Wrapper for slides -->
+	  <div class="carousel-inner">
+		<div class="item active">
+		  <img src="./img/slider/concert1.jpg" alt="Concert">
+		</div>
+
+		<div class="item">
+		  <img src="./img/slider/concert2.jpg" alt="Concert">
+		</div>
+
+		<div class="item">
+		  <img src="./img/slider/concert3.jpg" alt="Concert">
+		</div>
+		
+	  </div>
+
+	  <!-- Left and right controls -->
+	  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
+		<span class="glyphicon glyphicon-chevron-left"></span>
+		<span class="sr-only">Previous</span>
+	  </a>
+	  <a class="right carousel-control" href="#myCarousel" data-slide="next">
+		<span class="glyphicon glyphicon-chevron-right"></span>
+		<span class="sr-only">Next</span>
+	  </a>
+	</div>
+</div>
 </div>
 </body>
 </html>
