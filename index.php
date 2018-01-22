d1be660d549d440834b522c24b379e4874d31145
Pol <polcerdan@gmail.com>
2018-01-22 21:25:11 +0100

    Primera versio menu i header semi fet

--- index.php
+++ index.php
@@ -13,11 +13,40 @@
 <!-- Latest compiled and minified JavaScript -->
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 
+<link rel="stylesheet" type="text/css"href="./CSS/reset.css">
+<link rel="stylesheet" type="text/css"href="./CSS/style.css">
+
 <title> Jaharmony </title>
 </head>
 <body>
+<div class="container">
 <header>
-
+	<div class="row">
+		<div class="col-lg-4 centrat"> 
+			<div class="col-lg-4 logo"> 
+				
+			</div>
+			<div class="col-lg-8" style="position:relative;">
+				<img class="posicionar" src="./img/titol.png"></img>
+			</div>
+		</div>
+		<div class="col-lg-8 centrat" style="position:relative;"> 
+			<ul class="menu">
+				<li><a>About us</a></li>
+				<li><a>Brand</a>
+					<ul>
+						<li><a>Albert Cerdan </a></li>
+						<li><a> Dani Merme </a></li>
+						<li><a> Adrià </a></li>
+						<li><a> Nil Couto </a></li>
+					</ul>
+				</li>
+				<li><a>Concerts</a></li>
+				<li><a>Contact</a></li>
+			</ul>
+		</div>		
+	</div>
 </header>
+</div>
 </body>
 </html>
